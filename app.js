e3d9b4cf4524e8c0b2dff9cf5fe9146e5e3b4521
IrvinG-13 <irvigonza13@gmail.com>
2023-10-11 11:48:46 -0500

    Computer Logic and User Choose

--- app.js
+++ app.js
@@ -1,3 +1,50 @@
-function getComputerChoice(){
-    
+const getComputerChoice = ()=> {
+    let choice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+
+    if (choice == 1){
+        return "Rock";
+
+    }else if(choice == 2){
+        return "Paper";
+
+    }else if(choice == 3){
+
+        return "Scissors";
+    }  
 }
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == "Rock" && computerSelection == "Paper"){
+        return "You Lose! Paper beats Rock";
+
+    }else if(playerSelection == "Paper" && computerSelection == "Scissors"){
+        return "You Lose! Scissors beats Paper";
+
+    }else if(playerSelection == "Scissors" && computerSelection == "Rock"){
+        return "You Lose! Rock beats Scissors"
+
+    }else if(playerSelection == "Rock" && computerSelection == "Rock"){
+        return "Draw";
+
+    }else if(playerSelection == "Paper" && computerSelection == "Paper"){
+        return "Draw";
+
+    }else if(playerSelection == "Scissors" && computerSelection == "Scissors"){
+        return "Draw"
+
+    }else if(playerSelection == "Rock" && computerSelection == "Scissors"){
+        return "You Win! Rock beats Scissors";
+
+    }else if(playerSelection == "Scissors" && computerSelection == "Paper"){
+        return "You Win! Scissors beats Paper";
+        
+    }else if(playerSelection == "Paper" && computerSelection == "Rock"){
+        return "You Win! Paper beats Rock";
+        
+    }
+}
+
+
+const playerSelection = prompt("Choose");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
