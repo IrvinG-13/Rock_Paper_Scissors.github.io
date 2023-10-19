75fcb2fc8dbc70a484a1045f62e8ef7109c506c6
IrvinG-13 <irvigonza13@gmail.com>
2023-10-19 15:28:08 -0500

    Add player choice and logic result

--- app.js
+++ app.js
@@ -1,50 +1,62 @@
-const getComputerChoice = ()=> {
-    let choice = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+const getComputerChoice = () => {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * 3);
+    return choices[randomIndex];
+};
 
-    if (choice == 1){
-        return "Rock";
+let playerSelection; 
 
-    }else if(choice == 2){
-        return "Paper";
+const Rock = document.getElementById('rock');
+const Paper = document.getElementById('paper');
+const Scissors = document.getElementById('scissors');
+const resultElement = document.getElementById('result');
 
-    }else if(choice == 3){
+Rock.addEventListener('click', () => {
+    playerSelection = "Rock";
+    resolvePlayerSelection();
+});
 
-        return "Scissors";
-    }  
+Paper.addEventListener('click', () => {
+    playerSelection = "Paper";
+    resolvePlayerSelection();
+});
+
+Scissors.addEventListener('click', () => {
+    playerSelection = "Scissors";
+    resolvePlayerSelection();
+});
+
+function resolvePlayerSelection() {
+    const computerSelection = getComputerChoice();
+    const resultado = playRound(playerSelection, computerSelection);
+    resultElement.textContent = resultado;
 }
 
-function playRound(playerSelection, computerSelection){
-    if(playerSelection == "Rock" && computerSelection == "Paper"){
-        return "You Lose! Paper beats Rock";
 
-    }else if(playerSelection == "Paper" && computerSelection == "Scissors"){
-        return "You Lose! Scissors beats Paper";
-
-    }else if(playerSelection == "Scissors" && computerSelection == "Rock"){
-        return "You Lose! Rock beats Scissors"
-
-    }else if(playerSelection == "Rock" && computerSelection == "Rock"){
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == "Rock" && computerSelection == "Paper") {
+        return "You Lose!  Paper beats Rock ";
+    } else if (playerSelection == "Paper" && computerSelection == "Scissors") {
+        return "You Lose!  Scissors beats Paper";
+    } else if (playerSelection == "Scissors" && computerSelection == "Rock") {
+        return "You Lose!  Rock beats Scissors";
+    } else if (playerSelection == "Rock" && computerSelection == "Rock") {
         return "Draw";
-
-    }else if(playerSelection == "Paper" && computerSelection == "Paper"){
+    } else if (playerSelection == "Paper" && computerSelection == "Paper") {
         return "Draw";
-
-    }else if(playerSelection == "Scissors" && computerSelection == "Scissors"){
-        return "Draw"
-
-    }else if(playerSelection == "Rock" && computerSelection == "Scissors"){
-        return "You Win! Rock beats Scissors";
-
-    }else if(playerSelection == "Scissors" && computerSelection == "Paper"){
-        return "You Win! Scissors beats Paper";
-        
-    }else if(playerSelection == "Paper" && computerSelection == "Rock"){
-        return "You Win! Paper beats Rock";
-        
+    } else if (playerSelection == "Scissors" && computerSelection == "Scissors") {
+        return "Draw";
+    } else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+        return "You Win!  Rock beats Scissors";
+    } else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+        return "You Win!  Scissors beats Paper";
+    } else if (playerSelection == "Paper" && computerSelection == "Rock") {
+        return "You Win!  Paper beats Rock";
     }
 }
 
 
-const playerSelection = prompt("Choose");
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+
+
+
+
